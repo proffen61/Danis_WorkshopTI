efb1615071d9ec8558e7238898286d36809fe7b3
proffen61 <150996016+proffen61@users.noreply.github.com>
2023-11-23 15:45:20 +0700

    Update dashboard.php

--- resources/views/dashboard.php
+++ resources/views/dashboard.php
@@ -1,7 +1,13 @@
 <p>
         nama : Iman Danis Ramda Maryan
     </p>
+<p>
+    nim : 233307079
+</p>
     <p>
-        kelas / prodi : 1C/Teknologi Informasi
-    <p>
+        kelas : 1C
+    </p>
+<p>
+    prodi : Teknologi Informasi
+</p>
     
